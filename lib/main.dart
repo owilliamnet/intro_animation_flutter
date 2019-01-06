d039c652f6dcff46f3828cb4d452d6edd1cc2a41
William Martins <owilliam.net@gmail.com>
2019-01-05 23:28:47 -0200

    Implemented the chain animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'anim/counter_animation.dart';
+import 'package:intro_animations_flutter/anim/chain_animation.dart';
+//import 'anim/counter_animation.dart';
 
 void main() => runApp(MyApp());
 
@@ -24,11 +25,35 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+
+  Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+        duration: Duration(milliseconds: 1800), vsync: this);
+    animation = CurvedAnimation(parent: controller, curve: Curves.linear);
+    animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        controller.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        controller.forward();
+      }
+    });
+
+    controller.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: CounterAnimator()),
-    );
+        body: ChainAnimation(
+      animation: animation,
+    ));
+    //return Scaffold(body: Center(child: CounterAnimator()),);
   }
 }
